a5d62e4eae770d1e8ff9f6954166c2fd98501e4a
BradenE19 <6115@holbertonstudents.com>
2023-07-20 13:05:10 -0500

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,33 +6,34 @@ import sys
 
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
-    employee_todo_url = f"{base_url}/todos?userId={employee_id}"
-    response = requests.get(employee_todo_url)
+    employee_url = f"{base_url}/users/{employee_id}"
+    todos_url = f"{base_url}/todos?userId={employee_id}"
 
-    if response.status_code == 200:
-        todos = response.json()
-        completed_todos = [todo for todo in todos if todo["completed"]]
-        total_todos = len(todos)
-        completed_todos_count = len(completed_todos)
+    try:
+        employee_response = requests.get(employee_url)
+        todos_response = requests.get(todos_url)
 
-        progress_percentage = (completed_todos_count / total_todos) * 100
+        if employee_response.status_code == 200 and todos_response.status_code == 200:
+            employee_data = employee_response.json()
+            todos_data = todos_response.json()
 
-        return {
-            "total_todos": total_todos,
-            "completed_todos_count": completed_todos_count,
-            "progress_percentage": progress_percentage
-        }
-    else:
-        print(f"Failed to fetch TODO list for employee {employee_id}. Error code: {response.status_code}")
-        return None
+            employee_name = employee_data["name"]
+            total_tasks = len(todos_data)
+            completed_tasks = [task for task in todos_data if task["completed"]]
+            num_completed_tasks = len(completed_tasks)
+
+            print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+
+            for task in completed_tasks:
+                print(f"\t{task['title']}")
+
+        else:
+            print(f"Failed to fetch TODO list for employee {employee_id}. Error code: {employee_response.status_code}")
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred: {e}")
 
 if __name__ == "__main__":
-    employee_id = 1  # Replace this with the employee ID you want to query
-    employee_progress = get_employee_todo_progress(employee_id)
-
-    if employee_progress:
-        print(f"Employee {employee_id} TODO List Progress:")
-        print(f"Total Todos: {employee_progress['total_todos']}")
-        print(f"Completed Todos: {employee_progress['completed_todos_count']}")
-        print(f"Progress Percentage: {employee_progress['progress_percentage']}%")
-
+    employee_id = int(input("Enter the employee ID: "))
+    get_employee_todo_progress(employee_id)
+    
